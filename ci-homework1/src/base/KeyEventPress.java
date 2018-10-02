8157944c9018dbff6f50e5cf4652dce6198fec71
dattran <dattran1997@gmail.com>
2018-10-03 02:58:32 +0700

    homework3

--- ci-homework1/src/base/KeyEventPress.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public class KeyEventPress {
-    public  static  boolean isUpPress ;
-    public  static boolean isDownPress;
-    public  static boolean isRightPress;
-    public  static boolean isLeftPress;
-    public static boolean isSpacePress;
-}
--- ci-homework1/src/base/Setting.java
+++
@@ -1,8 +0,0 @@
-package base;
-
-public class Setting {
-    public static int  SCREEN_WIDTH = 800;
-    public static int  SCREEN_HEIGHT = 600;
-    public static int  START_PLAYER_POSITION_X = 200;
-    public static int  START_PLAYER_POSITION_Y = 300;
-}
